d373e03ceb066ccd19fb60f8900d5c919e33b43f
sf <sf@sfs-MacBook-Pro.local>
2021-12-08 20:32:50 +0300

    refactor: extrack weather routes into its own stack

--- scripts/routes/index.ts
+++ scripts/routes/index.ts
@@ -1,50 +1,45 @@
 import buildExtender from '@smartface/extension-utils/lib/router/buildExtender';
 import {
-  NativeRouter as Router,
-  NativeStackRouter as StackRouter,
-  Route,
+    NativeRouter as Router,
+    NativeStackRouter as StackRouter,
+    Route,
 } from '@smartface/router';
 import * as Pages from 'pages';
 import '@smartface/extension-utils/lib/router/goBack'; // Implements onBackButtonPressed
 
-const router = Router.of({
-  path: '/',
-  isRoot: true,
-  routes: [
-    StackRouter.of({
-      path: '/pages',
-      routes: [
-        Route.of({
-          path: '/pages/page1',
-          build: buildExtender({
-            getPageClass: () => Pages.Page1,
-            headerBarStyle: { visible: true },
-          }),
-        }),
-        Route.of({
-          path: '/pages/page2',
-          build: buildExtender({
-            getPageClass: () => Pages.Page2,
-            headerBarStyle: { visible: true },
-          }),
-        }),
+const weatherRouter = StackRouter.of({
+    path: '/pages',
+    routes: [
         Route.of({
             path: '/pages/pgHome',
             build: buildExtender({
-              getPageClass: () => Pages.PgHome,
-              headerBarStyle: { visible: false },
+                getPageClass: () => Pages.PgHome,
+                headerBarStyle: { visible: false },
             }),
-          }),
+        }),
         Route.of({
             path: '/pages/pgWeekly',
             build: buildExtender({
-              getPageClass: () => Pages.PgWeekly,
-              headerBarStyle: { visible: true },
+                getPageClass: () => Pages.PgWeekly,
+                headerBarStyle: { visible: true },
             }),
-          }),
-      ],
-    }),
-  ],
+        }),
+        Route.of({
+            path: '/pages/pgForecastWeekly',
+            build: buildExtender({
+                getPageClass: () => Pages.PgForecastWeekly,
+                headerBarStyle: { visible: true },
+            }),
+        }),
+    ],
+});
+
+const router = Router.of({
+    path: '/',
+    isRoot: true,
+    routes: [
+        weatherRouter
+    ],
 });
 
 export default router;
