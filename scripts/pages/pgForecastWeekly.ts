b612a6ae9925277943a7c7fb8d38703166cf2207
sf <sf@sfs-MacBook-Pro.local>
2021-12-09 09:10:55 +0300

    feat: fake-loader with ActivityLoader

--- scripts/pages/pgForecastWeekly.ts
+++ scripts/pages/pgForecastWeekly.ts
@@ -1,8 +1,14 @@
 import PgForecastWeeklyDesign from 'generated/pages/pgForecastWeekly';
 import Color from '@smartface/native/ui/color';
 import ForecastListItem from 'components/ForecastListItem';
+import Image from '@smartface/native/ui/image';
+import FlexLayout, { AlignItems, JustifyContent, PositionType } from '@smartface/native/ui/flexlayout';
+import Screen from '@smartface/native/device/screen';
+import ActivityIndicator from '@smartface/native/ui/activityindicator';
 
 export default class PgForecastWeekly extends PgForecastWeeklyDesign {
+    loaderView: FlexLayout
+
     routeData: {
         headerTitle: string
     }
@@ -12,6 +18,53 @@ export default class PgForecastWeekly extends PgForecastWeeklyDesign {
         this.onShow = onShow.bind(this, this.onShow.bind(this));
         // Overrides super.onLoad method
         this.onLoad = onLoad.bind(this, this.onLoad.bind(this));
+    }
+
+    initForecastList() {
+        this.forecastList.visible = false
+
+        this.forecastList.backgroundColor = Color.create('#282B39')
+        this.forecastList.rowHeight = 50
+
+
+        this.forecastList.onRowBind = (listViewItem: ForecastListItem) => {
+
+            listViewItem.day.text = 'Monday'
+            listViewItem.dayDegree.text = '32'
+            listViewItem.nightDegree.text = '32'
+
+            listViewItem.weatherTypeIcon.image = Image.createFromFile(WEATHER_ICON_TYPES[randomIntFromInterval(0, 2)])
+        }
+
+        this.forecastList.onPullRefresh = () => {
+            this.loaderView.visible = true
+            this.forecastList.visible = false
+            
+            setTimeout(() => {
+                this.loaderView.visible = false
+                this.forecastList.visible = true
+                this.forecastList.stopRefresh();
+            }, 1000);
+        }
+    }
+
+    initLoaderLayout() {
+        this.loaderView = new FlexLayout();
+        let container = this.loaderView
+        container.paddingTop = Screen.height / 4
+        container.width = Screen.width;
+        container.height = Screen.height
+        container.alignItems = AlignItems.CENTER
+        container.positionType = PositionType.ABSOLUTE
+        container.backgroundColor = Color.TRANSPARENT
+
+        let activityLoader = new ActivityIndicator();
+        activityLoader.color = Color.WHITE
+
+        container.addChild(activityLoader)
+
+        this.layout.addChild(container)
+        this.layout.applyLayout();
     }
 }
 
@@ -35,14 +88,22 @@ function onShow(this: PgForecastWeekly, superOnShow: () => void) {
 function onLoad(this: PgForecastWeekly, superOnLoad: () => void) {
     superOnLoad();
 
-    this.forecastList.backgroundColor = Color.create('#282B39')
-    this.forecastList.rowHeight = 50
+    this.initForecastList();
+    this.initLoaderLayout();
 
+    setTimeout(() => {
+        this.loaderView.visible = false
+        this.forecastList.visible = true
+    }, 2);
+}
 
-    this.forecastList.onRowBind = (listViewItem: ForecastListItem) => {
-        
-        listViewItem.day.text = 'Monday'
-        listViewItem.dayDegree.text = '32'
-        listViewItem.nightDegree.text = '32'
-    }
+const WEATHER_ICON_TYPES = [
+    'images://sunny.png',
+    'images://windy.png',
+    'images://rainy.png'
+]
+
+// https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
